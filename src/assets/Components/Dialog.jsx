832fb19fc4068af2b4a8578c7d4a2af09d26808e
Ali <alihajjhussein63@gmail.com>
2025-03-14 01:07:00 +0200

    Added: dialog animation

--- src/assets/Components/Dialog.jsx
+++ src/assets/Components/Dialog.jsx
@@ -1,32 +1,27 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 export default function Dialog({ isOpen, onClose, children }) {
     const dialogRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
 
     // Handle opening/closing the dialog
     useEffect(() => {
         if (isOpen) {
+            setIsVisible(true);
             dialogRef.current.showModal();
         } else {
-            dialogRef.current.close();
+            setIsVisible(false);
+            setTimeout(() => {
+                dialogRef.current.close();
+            }, 300); // Match the duration of the fade-out animation
         }
     }, [isOpen]);
-
-    // Close dialog on backdrop click
-    // const handleBackdropClick = (e) => {
-    //     if (e.target === dialogRef.current) {
-    //         onClose();
-    //     }
-    // };
 
     return (
         <dialog
             ref={dialogRef}
-            // onClick={handleBackdropClick}
-            className="p-6 rounded-lg shadow-lg backdrop:bg-black backdrop:opacity-50"
-        >
+            className={`p-6 rounded-lg shadow-lg backdrop:bg-black backdrop:opacity-50 ${isVisible ? 'drop-in' : 'drop-out'}`}>
             {children}
         </dialog>
     );
 };
-
